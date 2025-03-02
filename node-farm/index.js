577cb53ad24baa527d7832dfd6c457b16f4b6763
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-02 14:55:54 +0200

    acess file system

---
+++ node-farm/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const http = require('http');
+
+/* 
+TODO: Files
+const fileIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(fileIn);
+const fileOut = `Avocado informatons: ${fileIn}`;
+fs.writeFileSync('./txt/out.txt', fileOut);
+
+
+fs.readFile('./txt/start.txt', 'utf8', (err, data) => {
+  console.log(data);
+});
+
+*/
