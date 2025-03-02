a3ca6ceb8e8f67714af8bc4b9b86bd3b277a21f4
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-02 15:52:04 +0200

    handle simple routing

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 /* 
 TODO: Files
@@ -17,8 +18,29 @@ fs.readFile('./txt/start.txt', 'utf8', (err, data) => {
 
 // TODO: Server
 
+const json = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server');
+  const pathName = req.url;
+  console.log(pathName);
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('Hello from the OverView');
+  } else if (pathName === '/product') {
+    res.end('Hello from the Product');
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+
+    res.end(json);
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+    });
+
+    res.end('<h1>404 Page not found</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
