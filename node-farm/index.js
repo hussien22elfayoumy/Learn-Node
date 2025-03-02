ec2bb786309f6d3ea7e3fe386cd53d5f5fadf54f
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-02 16:52:31 +0200

    overview template

--- node-farm/index.js
+++ node-farm/index.js
@@ -18,23 +18,65 @@ fs.readFile('./txt/start.txt', 'utf8', (err, data) => {
 
 // TODO: Server
 
-const json = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+const tempOverview = fs.readFileSync(
+  `./templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(`./templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(
+  `./templates/template-product.html`,
+  'utf-8'
+);
+
+const jsonData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(jsonData);
+
+function replaceTemp(temp, el) {
+  let output = temp;
+  output = output.replaceAll('{%IMAGE%}', el.image);
+  output = output.replaceAll('{%PRODUCTNAME%}', el.productName);
+  output = output.replaceAll('{%QUANTITY%}', el.quantity);
+  output = output.replaceAll('{%PRICE%}', el.price);
+  output = output.replaceAll('{%ID%}', el.id);
+  output = output.replaceAll('{%DESCRIPTION%}}', el.description);
+  if (el.organic) output = output.replaceAll('{%NOT_ORGANIC%}', 'not-organic');
+
+  return output;
+}
+
+function filledOverview() {
+  const tempCardHtml = dataObj.map((el) => replaceTemp(tempCard, el)).join(',');
+
+  return tempOverview.replace('{%PRODUCT_CARDS%}', tempCardHtml);
+}
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
   console.log(pathName);
 
+  // NOTE: Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('Hello from the OverView');
-  } else if (pathName === '/product') {
-    res.end('Hello from the Product');
-  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    res.end(filledOverview());
+  }
+
+  // NOTE: Product page
+  else if (pathName === '/product') {
+    res.end('');
+  }
+
+  // NOTE: api route
+  else if (pathName === '/api') {
     res.writeHead(200, {
       'Content-type': 'application/json',
     });
+    res.end(jsonData);
+  }
 
-    res.end(json);
-  } else {
+  // NOTE: Not found page
+  else {
     res.writeHead(404, {
       'Content-type': 'text/html',
     });
