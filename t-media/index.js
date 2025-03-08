d2fb2a8994f955270e67d5bd81604381e3d06243
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-08 19:57:49 +0200

    create a server

--- t-media/index.js
+++ t-media/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import http from 'http';
+const PORT = process.env.PORT;
+const server = http.createServer((req, res) => {
+  res.end('hello again');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
