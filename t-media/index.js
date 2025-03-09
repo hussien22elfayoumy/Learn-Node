bb9cc769a788dec3e8a9aaa3028139a063244b41
hussien22elfayoumy <hussienmedo36@gmail.com>
2025-03-09 14:34:04 +0200

    update

--- t-media/index.js
+++ t-media/index.js
@@ -1,7 +1,23 @@
-import http from 'http';
+import { createServer } from 'http';
 const PORT = process.env.PORT;
-const server = http.createServer((req, res) => {
-  res.end('hello again');
+
+const users = [
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jean Doe' },
+  { id: 3, name: 'Jim Doe' },
+];
+
+const server = createServer((req, res) => {
+  if (req.url === '/api/users' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json');
+    res.write(JSON.stringify(users));
+    res.end();
+  } else {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 404;
+    res.write(JSON.stringify({ message: 'Route not found' }));
+    res.end();
+  }
 });
 
 server.listen(PORT, () => {
